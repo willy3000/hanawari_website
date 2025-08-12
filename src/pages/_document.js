53bd164639cb7d99380670093ce790ca548ca1f5
Jillo Wario <willywario0@gmail.com>
2025-08-12 22:52:33 +0300

    sitemaps and robots files

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="assets/logo-bg.png" />
-        <title>Home - Hanawari</title>
+        <link rel="icon" href="/assets/logo-bg.png" />
+        <title>Hanawari Salsa – Premium Homemade Salsa in Kenya</title>
+        <meta
+          name="description"
+          content="Premium homemade salsa made with fresh ingredients. Order online for fast delivery."
+        />
       </Head>
       <body className="antialiased">
         <Main />
